a5bf3b6915c39254cc4657b8ad3f737c4156b2d9
janitha-chamara <janithah@insharptechnologies.com>
2022-09-30 00:20:40 +0530

    searchs for table

--- src/components/project-plan/ProjectsTable.vue
+++ src/components/project-plan/ProjectsTable.vue
@@ -8,7 +8,17 @@
         row-key="name"
         flat
         bordered
-    />
+    >
+      <template v-slot:body-cell-status="props">
+        <q-td :props="props">
+          <q-linear-progress size="25px" :value="getProgress" color="accent">
+            <div class="absolute-full flex flex-center">
+              <q-badge color="white" text-color="accent" :label="getLabel" />
+            </div>
+          </q-linear-progress>
+        </q-td>
+      </template>
+    </q-table>
   </div>
 </template>
 
@@ -21,21 +31,29 @@ export default {
   data(){
     return {
       columns: [
-        { name: 'job', align: 'center', label: 'Job', field: 'jobName', sortable: true },
-        { name: 'business_unit', align: 'center', label: 'Business Unit', field: 'businessUnitID', sortable: true },
-        { name: 'client', align: 'center', label: 'Client', field: 'clientID', sortable: true },
-        { name: 'project_manager', align: 'center', label: 'Project Manager', field: 'projectMangerID', sortable: true },
+        { name: 'job', align: 'center', label: 'Job', field: 'job_name', sortable: true },
+        { name: 'business_unit', align: 'center', label: 'Business Unit', field: 'business_unit', sortable: true },
+        { name: 'client', align: 'center', label: 'Client', field: 'client', sortable: true },
+        { name: 'project_manager', align: 'center', label: 'Project Manager', field: 'project_manager', sortable: true },
         { name: 'sdm', align: 'center', label: 'SDM', field: 'sdm', sortable: true },
-        { name: 'status', align: 'center', label: 'Status', field: 'statusID', sortable: true },
-        { name: 'quoted_hours', align: 'center', label: 'Quoted Hours', field: 'quotedHours', sortable: true },
-        { name: 'actual_hours', align: 'center', label: 'Actual Hours', field: 'actualHours', sortable: true },
-        { name: 'percent_used', align: 'center', label: 'Percent Used', field: 'percentUsed', sortable: true },
-        { name: 'estimate_to_complete', align: 'center', label: 'Est To Complete', field: 'estToComplHours', sortable: true },
-        { name: 'percent_completed', align: 'center', label: 'Percent Completed', field: 'percentComplete', sortable: true },
-        { name: 'difference_percent', align: 'center', label: 'Difference Percent', field: 'differencePercent', sortable: true },
-        { name: 'forecast_hours', align: 'center', label: 'Forecast Hours', field: 'forecastHours', sortable: true },
-        { name: 'variance_hours', align: 'center', label: 'Variance Hours', field: 'varianceHours', sortable: true },
+        { name: 'status', align: 'center', label: 'Status', field: 'status', sortable: true },
+        { name: 'quoted_hours', align: 'center', label: 'Quoted Hours', field: 'quoted_hours', sortable: true },
+        { name: 'actual_hours', align: 'center', label: 'Actual Hours', field: 'actual_hours', sortable: true },
+        { name: 'percent_used', align: 'center', label: 'Percent Used', field: 'percent_used', sortable: true },
+        { name: 'estimate_to_complete', align: 'center', label: 'Est To Complete', field: 'estimate_to_complete_hours', sortable: true },
+        { name: 'percent_completed', align: 'center', label: 'Percent Completed', field: 'percent_complete', sortable: true },
+        { name: 'difference_percent', align: 'center', label: 'Difference Percent', field: 'difference_percent', sortable: true },
+        { name: 'forecast_hours', align: 'center', label: 'Forecast Hours', field: 'forecast_hours', sortable: true },
+        { name: 'variance_hours', align: 'center', label: 'Variance Hours', field: 'variance_hours', sortable: true },
       ]
+    }
+  },
+  methods: {
+    getProgress(){
+      return 0.3
+    },
+    getLabel(){
+      return '30%'
     }
   }
 }
--- src/views/ProjectPlanView.vue
+++ src/views/ProjectPlanView.vue
@@ -3,13 +3,13 @@
     <q-card-section class="space-y-2">
       <div class="font-medium">Search projects</div>
       <div class="flex space-x-5">
-        <q-input dense outlined v-model="text" label="Job NO" class="w-[180px]"/>
-        <q-select dense outlined v-model="model" :options="options" class="w-[180px]" label="Job Name"/>
-        <q-select dense outlined v-model="model" :options="options" class="w-[180px]" label="Client"/>
-        <q-select dense outlined v-model="model" :options="options" class="w-[180px]" label="Business Unit"/>
-        <q-select dense outlined v-model="model" :options="options" class="w-[180px]" label="Project Manager"/>
-        <q-select dense outlined v-model="model" :options="options" class="w-[180px]" label="Service Delivery Manager"/>
-        <q-select dense outlined v-model="model" :options="options" class="w-[180px]" label="Status"/>
+        <q-input dense outlined label="Job NO" class="w-[180px]" v-model="jobNoSearchText"/>
+        <q-input dense outlined v-model="jobNameSearchText"  class="w-[180px]" label="Job Name"/>
+        <q-input dense outlined v-model="clientSearchText"  class="w-[180px]" label="Client"/>
+        <q-input dense outlined v-model="businessUnitSearchText"  class="w-[180px]" label="Business Unit"/>
+        <q-input dense outlined v-model="projectManagerSearchText"  class="w-[180px]" label="Project Manager"/>
+        <q-input dense outlined v-model="serviceDeliveryManagerSearchText"  class="w-[180px]" label="Service Delivery Manager"/>
+        <q-input dense outlined v-model="statusSearchText" class="w-[180px]" label="Status"/>
       </div>
     </q-card-section>
     <q-card-section>
@@ -46,7 +46,32 @@ export default {
           forecast_hours: 'Sample',
           variance_hours: 'Sample',
         },
-      ]
+      ],
+      originalProjects: [
+        {
+          job: 'Sample',
+          business_unit: 'Sample',
+          client: 'Sample',
+          project_manager: 'Sample',
+          sdm: 'Sample',
+          status: 'Sample',
+          quoted_hours: 'Sample',
+          actual_hours: 'Sample',
+          percent_used: 'Sample',
+          estimate_to_complete: 'Sample',
+          percent_completed: 'Sample',
+          difference_percent: 'Sample',
+          forecast_hours: 'Sample',
+          variance_hours: 'Sample',
+        },
+      ],
+      jobNoSearchText: '',
+      jobNameSearchText: '',
+      clientSearchText: '',
+      businessUnitSearchText: '',
+      projectManagerSearchText: '',
+      serviceDeliveryManagerSearchText: '',
+      statusSearchText: '',
     }
   },
 
@@ -54,10 +79,91 @@ export default {
     this.fetchJobs()
   },
 
+  watch: {
+    jobNoSearchText(newValue){
+      if (newValue === ''){
+        this.projects = [...this.originalProjects]
+
+        return
+      }
+
+      this.projects = [...this.projects.filter(project => {
+        return project.job_name.toString().includes(newValue)
+      })]
+    },
+    jobNameSearchText(newValue){
+      if (newValue === ''){
+        this.projects = [...this.originalProjects]
+
+        return
+      }
+
+      this.projects = [...this.projects.filter(project => {
+        return project.job_name.toString().includes(newValue)
+      })]
+    },
+    clientSearchText(newValue){
+      if (newValue === ''){
+        this.projects = [...this.originalProjects]
+
+        return
+      }
+
+      this.projects = [...this.projects.filter(project => {
+        return project.client.toString().includes(newValue)
+      })]
+    },
+    businessUnitSearchText(newValue){
+      if (newValue === ''){
+        this.projects = [...this.originalProjects]
+
+        return
+      }
+
+      this.projects = [...this.projects.filter(project => {
+        return project.business_unit.toString().includes(newValue)
+      })]
+    },
+    projectManagerSearchText(newValue){
+      if (newValue === ''){
+        this.projects = [...this.originalProjects]
+
+        return
+      }
+
+      this.projects = [...this.projects.filter(project => {
+        return project.project_manager.toString().includes(newValue)
+      })]
+    },
+    serviceDeliveryManagerSearchText(newValue){
+      if (newValue === ''){
+        this.projects = [...this.originalProjects]
+
+        return
+      }
+
+      this.projects = [...this.projects.filter(project => {
+        return project.account_manager.toString().includes(newValue)
+      })]
+    },
+    statusSearchText(newValue){
+      if (newValue === ''){
+        this.projects = [...this.originalProjects]
+
+        return
+      }
+
+      this.projects = [...this.projects.filter(project => {
+        return project.status.toString().includes(newValue)
+      })]
+    },
+  },
+
   methods: {
     async fetchJobs() {
-      const {data} = await Http.get('Job/GetAllJobs')
-      this.projects = [...data.response]
+      const {data} = await Http.get('jobs')
+      this.projects = [...data.data]
+      this.originalProjects = [...data.data]
     }
   }
 }
